5d782679ddd86cfc876bc095a1c69b4ecfb020a4
Muhammad Afif Pratama Nugraha <123240031@student.upnyk.ac.id>
2025-11-11 14:06:05 +0700

    Add files via upload

---
+++ vete_run.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Nov 2025 pada 08.04
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vete_run`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori_lomba`
+--
+
+CREATE TABLE `kategori_lomba` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) DEFAULT NULL,
+  `jarak_km` int(11) DEFAULT NULL,
+  `biaya` decimal(10,2) DEFAULT NULL,
+  `kuota` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kategori_lomba`
+--
+
+INSERT INTO `kategori_lomba` (`id_kategori`, `nama_kategori`, `jarak_km`, `biaya`, `kuota`) VALUES
+(1, '5K', 5, 50000.00, 100),
+(2, '15K', 15, 100000.00, 100),
+(3, '50K', 50, 200000.00, 50);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pendaftaran` int(11) DEFAULT NULL,
+  `tanggal_bayar` datetime DEFAULT current_timestamp(),
+  `jumlah_bayar` decimal(10,2) DEFAULT NULL,
+  `metode_bayar` enum('transfer','cash') DEFAULT 'transfer',
+  `bukti_transfer` varchar(255) DEFAULT NULL,
+  `status_bayar` enum('menunggu','terverifikasi','ditolak') DEFAULT 'menunggu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pendaftaran`
+--
+
+CREATE TABLE `pendaftaran` (
+  `id_pendaftaran` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `tanggal_daftar` datetime DEFAULT current_timestamp(),
+  `no_bib` varchar(20) DEFAULT NULL,
+  `status_daftar` enum('menunggu','disetujui','ditolak') DEFAULT 'menunggu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pendaftaran`
+--
+
+INSERT INTO `pendaftaran` (`id_pendaftaran`, `id_user`, `id_kategori`, `tanggal_daftar`, `no_bib`, `status_daftar`) VALUES
+(2, 3, 1, '2025-11-10 11:45:40', NULL, 'menunggu');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `no_hp` varchar(20) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `role` enum('admin','user') DEFAULT 'user',
+  `tanggal_daftar` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id_user`, `nama_lengkap`, `email`, `password`, `no_hp`, `alamat`, `role`, `tanggal_daftar`) VALUES
+(1, 'Admin VETE-RUN', 'admin@veterun.com', '$2y$10$OKf89kt7bdk.0BgGSCkyUOlzcyHKMpnI1qaOeoAaMyaqlDF4CMUBS', NULL, NULL, 'admin', '2025-11-09 20:29:47'),
+(3, 'apip penyet', 'test@gmail.com', '$2y$10$/P.nczdfbAP.FuBEDChKkuILnZUKbZYPiGCNo5laPueC5VmXDbH12', NULL, NULL, 'user', '2025-11-10 10:04:40');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kategori_lomba`
+--
+ALTER TABLE `kategori_lomba`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_pendaftaran` (`id_pendaftaran`);
+
+--
+-- Indeks untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD PRIMARY KEY (`id_pendaftaran`),
+  ADD UNIQUE KEY `no_bib` (`no_bib`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori_lomba`
+--
+ALTER TABLE `kategori_lomba`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_pendaftaran`) REFERENCES `pendaftaran` (`id_pendaftaran`) ON DELETE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pendaftaran`
+--
+ALTER TABLE `pendaftaran`
+  ADD CONSTRAINT `pendaftaran_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE,
+  ADD CONSTRAINT `pendaftaran_ibfk_2` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_lomba` (`id_kategori`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
